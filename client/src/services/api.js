8e9e58103994b9ed4a8e11f0cf47c98665c9048f
sai shankar <saishankarpaidi@gmail.com>
2026-01-02 18:01:20 +0530

    Configure API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://clinic-website-project.onrender.com/api"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default API;
