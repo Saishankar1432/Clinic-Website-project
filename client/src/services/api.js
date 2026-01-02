0f7aad5a7ff0f8220d3bf9901b7b2620638ba425
sai shankar <saishankarpaidi@gmail.com>
2026-01-02 16:19:56 +0530

    Add health check root route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://clinic-website-project.onrender.com/api"
 });
 
 export default API;
